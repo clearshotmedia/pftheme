f1b0895fd2624806f36f42432d364fc30fbf7403
clearshotmedia <lara@clearshotmedia.com.au>
2020-04-24 15:12:58 +1000

    first transfer to dev

---
+++ modules/fw_image_twocols.php
@@ -0,0 +1,24 @@
+<?php
+
+$background = get_sub_field('background_image');
+$content_left = get_sub_field('content_left');
+$content_right = get_sub_field('content_right');
+?>
+
+<section id="fw-image-twocols" style="background:url('<?php echo $background; ?>'); background-size:cover;">
+    <div class="container">
+        <div class="row ">
+
+            <div class="col-md-7 py-3">
+
+            <p><?php echo $content_left; ?></p>
+            </div>
+            <div class="col-md-5 py-3">
+
+<p><?php echo $content_right; ?></p>
+</div>
+            </div>
+
+    </div>
+
+</section>
