0098014fef88140576525618d772ab3308075a27
clearshotmedia <lara@clearshotmedia.com.au>
2020-05-06 14:26:31 +1000

    mo

--- modules/gallery.php
+++ modules/gallery.php
@@ -6,23 +6,38 @@ $heading = get_sub_field('gallery_heading');
 <div class="container">
     <div class="row align-center">
         <div class="col-12">
-                <h2> <?php echo $heading; ?></h2>
+                <h1> <?php echo $heading; ?></h1>
+                <div class="tagline"><?php echo the_field('tagline', 'option');?></div>
                 <hr class="primary">
         </div>
     </div>
-
-    <div class="row">
+    <?php 
+			 if( have_rows('links') ): ?> 
+<div class="row">
     <div class="col-12">
-    <div id="filters" class="filter-group">  <button class="filter is-checked" data-filter="*">All Projects</button>
-            <button class="filter" data-filter=".residential">Residential</button>
-            <button class="filter" data-filter=".commerical">Commercial Projects</button>
-            <button class="filter" data-filter=".community">Community Living</button>
-            <button class="filter" data-filter=".lifestyle">Lifestyle Resort</button>
-            <button class="filter" data-filter=".project">Project Builds</button>
-            <button class="filter" data-filter=".bespoke">Custom/Bespoke Builds</button>
-            </div>
+    <div id="filters" class="filter-group">
+<?php while ( have_rows('links') ) : the_row(); 
+$link = get_sub_field('link');
+
+$link_url = $link['url'];
+    $link_title = $link['title'];
+?>
+      <a href="<?php echo esc_url( $link_url ); ?>"><button class="filter"><?php echo esc_html( $link_title ); ?></button></a>
+            
+            
+    <?php
+
+ 
+
+   endwhile;
+    ?> 
+    </div>
         </div>
     </div>
+    <?php
+    endif;
+    ?>
+
 </div>
     <div class=""container-fluid">
         <div class="row">
@@ -43,7 +58,7 @@ $heading = get_sub_field('gallery_heading');
                         setup_postdata( $post ); 
             ?>
                 <div class="gallery-item five <?php echo the_sub_field('image_tag'); ?>"> <div class="button-inner">
-                <a href="<?php the_permalink(); ?>">
+                <a href="<?php the_permalink($post_object->ID); ?>">
                 <div class="content-overlay"></div>
                      <img src="<?php echo the_sub_field('image'); ?>"> 
                      <div class="content-details fadeIn-top">
